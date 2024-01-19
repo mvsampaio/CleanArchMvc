3101777bc92f6f002a139c22b73396413523b783
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-01-19 12:40:17 -0300

    defining Category class validation and enriching the domain

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,17 +1,42 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    internal class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
-        public string Name { get; set; }
+        public string Name { get; private set; }        
 
-        public ICollection<Product> products { get; set; }
+        public Category(string name)
+        {
+            ValidationDomain(name);            
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidationDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
