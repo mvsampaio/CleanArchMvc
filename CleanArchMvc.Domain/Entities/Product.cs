006e84764cf3a600144bcbebc902f27a9aa1da8f
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-01-23 11:17:00 -0300

    Configuring entities using Fluent API

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -1,10 +1,4 @@
 ﻿using CleanArchMvc.Domain.Validation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
