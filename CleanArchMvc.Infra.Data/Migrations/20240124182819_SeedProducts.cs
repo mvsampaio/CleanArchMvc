9e41961f4ad561421e2ee1cf10bba804ac786548
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-01-24 17:23:00 -0300

    Applied to migrations

---
+++ CleanArchMvc.Infra.Data/Migrations/20240124182819_SeedProducts.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" +
+                "VALUES('Caderno espiral','Caderno espiral 100 folhas',7.45,50,'caderno1.png',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" +
+                "VALUES('Estojo escolar','Estojo escolar cinza',5.65,70,'estojo1.png',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" +
+                "VALUES('Borracha escolar','Borracha escolar branca pequena',3.25,80,'borracha1.png',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" +
+                "VALUES('Calculadora escolar','Calculadora escolar simples',15.39,20,'calculadora1.png',2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
